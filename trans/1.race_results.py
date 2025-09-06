9729b8497b3a0e819475f4a850606f3360f8f22b
rachitj <rachitj>
2025-09-06 13:01:05 +0000

    incremental load _rest of presentation layer

--- trans/1.race_results.py
+++ trans/1.race_results.py
@@ -1,10 +1,19 @@
 # Databricks notebook source
+dbutils.widgets.text("p_file_date", "2021-03-21")
+v_file_date = dbutils.widgets.get("p_file_date")
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ##### Read all the data as required
 
 # COMMAND ----------
 
 # MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/common_functions"
 
 # COMMAND ----------
 
@@ -32,7 +41,10 @@ races_df = spark.read.parquet(f"{processed_folder_path}/races") \
 # COMMAND ----------
 
 results_df = spark.read.parquet(f"{processed_folder_path}/results") \
-.withColumnRenamed("time", "race_time") 
+.filter(f"file_date = '{v_file_date}'") \
+.withColumnRenamed("time", "race_time") \
+.withColumnRenamed("race_id", "result_race_id") \
+.withColumnRenamed("file_date", "result_file_date") 
 
 # COMMAND ----------
 
@@ -51,7 +63,7 @@ race_circuits_df = races_df.join(circuits_df, races_df.circuit_id == circuits_df
 
 # COMMAND ----------
 
-race_results_df = results_df.join(race_circuits_df, results_df.race_id == race_circuits_df.race_id) \
+race_results_df = results_df.join(race_circuits_df, results_df.result_race_id == race_circuits_df.race_id) \
                             .join(drivers_df, results_df.driver_id == drivers_df.driver_id) \
                             .join(constructors_df, results_df.constructor_id == constructors_df.constructor_id)
 
@@ -61,26 +73,19 @@ from pyspark.sql.functions import current_timestamp
 
 # COMMAND ----------
 
-final_df = race_results_df.select("race_year", "race_name", "race_date", "circuit_location", "driver_name", "driver_number", "driver_nationality",
-                                 "team", "grid", "fastest_lap", "race_time", "points", "position") \
-                          .withColumn("created_date", current_timestamp())
+final_df = race_results_df.select("race_id", "race_year", "race_name", "race_date", "circuit_location", "driver_name", "driver_number", "driver_nationality",
+                                 "team", "grid", "fastest_lap", "race_time", "points", "position", "result_file_date") \
+                          .withColumn("created_date", current_timestamp()) \
+                          .withColumnRenamed("result_file_date", "file_date")
 
 # COMMAND ----------
 
-display(final_df.filter("race_year == 2020 and race_name == 'Abu Dhabi Grand Prix'").orderBy(final_df.points.desc()))
-
-# COMMAND ----------
-
-final_df.write.mode("overwrite").format("parquet").saveAsTable("f1_presentation.race_results")
+overwrite_partition(final_df, 'f1_presentation', 'race_results', 'race_id')
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC SELECT * from f1_presentation.race_results LIMIT 10;
-
-# COMMAND ----------
-
-# MAGIC %sql DESC DATABASE f1_presentation;
+# MAGIC Select * from f1_presentation.race_results;
 
 # COMMAND ----------
 
--- trans/2.driver_standings.py
+++ trans/2.driver_standings.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC ##### Produce constructor standings
+# MAGIC ##### Produce driver standings
 
 # COMMAND ----------
 
@@ -9,11 +9,11 @@ v_file_date = dbutils.widgets.get("p_file_date")
 
 # COMMAND ----------
 
-# MAGIC %run "../includes/configuration"
+# MAGIC %run "../includes/common_functions"
 
 # COMMAND ----------
 
-# MAGIC %run "../includes/common_functions"
+# MAGIC %run "../includes/configuration"
 
 # COMMAND ----------
 
@@ -40,8 +40,8 @@ race_results_df = spark.read.parquet(f"{presentation_folder_path}/race_results")
 
 from pyspark.sql.functions import sum, when, count, col
 
-constructor_standings_df = race_results_df \
-.groupBy("race_year", "team") \
+driver_standings_df = race_results_df \
+.groupBy("race_year", "driver_name", "driver_nationality", "team") \
 .agg(sum("points").alias("total_points"),
      count(when(col("position") == 1, True)).alias("wins"))
 
@@ -50,9 +50,18 @@ constructor_standings_df = race_results_df \
 from pyspark.sql.window import Window
 from pyspark.sql.functions import desc, rank, asc
 
-constructor_rank_spec = Window.partitionBy("race_year").orderBy(desc("total_points"), desc("wins"))
-final_df = constructor_standings_df.withColumn("rank", rank().over(constructor_rank_spec))
+driver_rank_spec = Window.partitionBy("race_year").orderBy(desc("total_points"), desc("wins"))
+final_df = driver_standings_df.withColumn("rank", rank().over(driver_rank_spec))
 
 # COMMAND ----------
 
-overwrite_partition(final_df, 'f1_presentation', 'constructor_standings', 'race_year')
+overwrite_partition(final_df, 'f1_presentation', 'driver_standings', 'race_year')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC Select * from f1_presentation.driver_standings;
+
+# COMMAND ----------
+
+
